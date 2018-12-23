463c42ce01325b5092703fb661eb6c952df95fa4
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2018-12-23 11:31:31 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+Malicioustm copyright arrow_down            
+const devs = ['446999223183998987' , '446999223183998987' , '446999223183998987' , '446999223183998987'];
+          const adminprefix = ".";
+          client.on('message', message => {
+              var argresult = message.content.split().slice(1).join(' ');
+                if (!devs.includes(message.author.id)) return;
+
+            if (message.content.startsWith(adminprefix + 'ply')) {
+              client.user.setGame(argresult);
+                message.channel.sendMessage(white_check_mark   ${argresult})
+            } else
+            if (message.content.startsWith(adminprefix + 'wt')) {
+            client.user.setActivity(argresult, {type:'WATCHING'});
+                message.channel.sendMessage(white_check_mark   ${argresult})
+            } else
+            if (message.content.startsWith(adminprefix + 'ls')) {
+            client.user.setActivity(argresult , {type:'LISTENING'});
+                message.channel.sendMessage(white_check_mark   ${argresult})
+            } else
+            if (message.content.startsWith(adminprefix + 'st')) {
+              client.user.setGame(argresult, "https://www.twitch.tv/abdoufersaoui_dz");
+                message.channel.sendMessage(white_check_mark   ${argresult})
+            }
+            });
+           
+
+client.login(process.env.BOT_TOKEN);
