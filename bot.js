c98f66b9ab0bb9b4d51626a7a0f5e09f74161a0a
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2018-12-23 11:55:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.on('ready', () => {
+console.log(Logged in as ${client.user.tag}!);
+});
 
-const devs = ['446999223183998987' , '446999223183998987' , '446999223183998987' , '446999223183998987'];
-          const adminprefix = ".";
-          client.on('message', message => {
-              var argresult = message.content.split().slice(1).join(' ');
-                if (!devs.includes(message.author.id)) return;
+const prefix = "."
+var adminprefix = '.'
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '446999223183998987') return;
 
-            if (message.content.startsWith(adminprefix + 'ply')) {
-              client.user.setGame(argresult);
-                message.channel.send(white_check_mark   ${argresult})
-            } else
-            if (message.content.startsWith(adminprefix + 'wt')) {
-            client.user.setActivity(argresult, {type:'WATCHING'});
-                message.channel.send(white_check_mark   ${argresult})
-            } else
-            if (message.content.startsWith(adminprefix + 'ls')) {
-            client.user.setActivity(argresult , {type:'LISTENING'});
-                message.channel.send(white_check_mark   ${argresult})
-            } else
-            if (message.content.startsWith(adminprefix + 'st')) {
-              client.user.setGame(argresult, "https://www.twitch.tv/abdoufersaoui_dz");
-                message.channel.send(white_check_mark   ${argresult})
-            }
-            });
-           
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(white_check_mark  : ${argresult})
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(white_check_mark  : ${argresult})
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(white_check_mark  : ${argresult})
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(white_check_mark  : ${argresult})
+}
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(Changing The Name To ..${argresult})
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(Changing The Avatar To :${argresult});
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN);
