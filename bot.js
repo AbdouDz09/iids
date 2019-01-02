17149cfa007dcb47142dd58b9e495f041026dd5f
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-02 20:26:04 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Abdou_Dz ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1help | OG Music`,"http://twitch.tv/abdoufersaoui_dz")
+client.user.setGame(`1play | KrK Music`,"http://twitch.tv/abdoufersaoui_dz")
 client.user.setStatus("dnd")
 });
 
